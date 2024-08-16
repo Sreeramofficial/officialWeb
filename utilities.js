e2e41da1fa7e63d8785c25a00c9ca86859d193f0
Sreeramofficial <mssreerammanatt@gmail.com>
2024-08-16 17:35:35 +0530

    precision  added

--- utilities.js
+++ utilities.js
@@ -6,10 +6,14 @@ function handleFormSubmission(event) {
   var isArrayStart = document.getElementById("startArray");
   var subscribeCheckbox = document.getElementById("gridCheck");
   var isArrayEnd = document.getElementById("isarraychecked");
-  var isRandomText = document.getElementById("randomText");
-  var isRandomNumbers = document.getElementById("randomNumbers");
+  var isRandomText = document.getElementById("inputforcheck1");
+  var isRandomNumbers = document.getElementById("inputforcheck2");
+  var precision;
   if (isRandomText.checked || isRandomNumbers.checked) {
-    value = handleCheckBox(isRandomText, isRandomNumbers);
+   
+    precision = document.getElementById("valueForInput").value;
+    console.log(precision);
+     value = handleCheckBox(isRandomText, isRandomNumbers,precision);
   }
 
   checkIsEmpty(key, value);
@@ -20,7 +24,9 @@ window.onload = function () {
     .addEventListener("submit", handleFormSubmission);
 };
 function checkIsEmpty(name, email) {
-  if (document.getElementById("output").textContent == "please validate json") {
+  if (document.getElementById("output").textContent == "PLEASE ADD NEW JSON") {
+    document.getElementById("outputTitle").innerHTML = "Your JSON Is";
+
     document.getElementById("output").innerHTML =
       "{<br> " + '"' + name + '"' + ":" + '"' + email + '"' + "<br>}";
   } else {
@@ -29,19 +35,10 @@ function checkIsEmpty(name, email) {
 }
 function handleJson(name, email) {
   var text = document.getElementById("output").innerHTML.replace("}", "");
+  document;
 
   document.getElementById("output").innerHTML =
-    "<br>" +
-    text +
-    ",<br>" +
-    '"' +
-    name +
-    '"' +
-    " : " +
-    '"' +
-    email +
-    '"' +
-    "<br> } ";
+    text + ",<br>" + '"' + name + '"' + " : " + '"' + email + '"' + "<br> } ";
 }
 function generateRandomString(length) {
   const characters =
@@ -60,15 +57,37 @@ function generateRandomNumbers(length) {
   }
   return result;
 }
-function handleCheckBox(isRandomText, isRandomNumbers) {
+function handleCheckBox(isRandomText, isRandomNumbers,precision) {
+  console.log("pre "+precision)
+  precision=precision==null?2:precision;
   if (isRandomText.checked || isRandomNumbers.checked) {
     if (isRandomText.checked) {
-      return generateRandomString(10);
+      return generateRandomString(precision);
       document.getElementById("randomText").value = false;
     } else {
-      return generateRandomNumbers(10);
+      return generateRandomNumbers(precision);
       document.getElementById("randomNumbers").value = false;
     }
   }
   return value;
+}
+function clearJson(event) {
+  event.preventDefault();
+  document.getElementById("outputTitle").innerHTML = null;
+  document.getElementById("output").innerHTML = "PLEASE ADD NEW JSON";
+}
+function displayPrecisionBox() {
+  console.log("methods call")
+
+
+  var styles= document.getElementById("valueForInput");
+
+  if (
+    document.getElementById("inputforcheck1").checked == true ||
+    document.getElementById("inputforcheck2").checked == true
+  ) {
+    styles.style.display = "block";
+  } else {
+    styles.style.display = "none";
+  }
 }
